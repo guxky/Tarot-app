b478925206b57c7d862e5444838071ad8dedaa4c
guxky <samuelbangueroortega@gmail.com>
2026-09-07 19:42:42 -0500

    Iniciando el front con vite

---
+++ frontend/src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+createRoot(document.getElementById('root')!).render(
+  <React.Fragment>
+    <button>MANUELA PERRA</button>
+    <button>MANUELA PERRA</button>
+    <button>MANUELA PERRA</button>
+  </React.Fragment>
+)
